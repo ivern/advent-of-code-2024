b2e4d97a2d12a4071c86f660428b0af48145018c
Javier Fernandez-Ivern <ivern@netflix.com>
2024-12-01 10:26:57 -0600

    Day 1 solutions

--- src/Puzzle1.java
+++ src/Puzzle1.java
@@ -1,21 +1,33 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 
 @SuppressWarnings("unused")
 public class Puzzle1 {
 
-    public int solve() {
-        try (var lines = Files.lines(Paths.get("./data/day1.txt"))) {
-            return lines.mapToInt(this::getOriginalNumber).sum();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    public int solve() throws IOException {
+        var lines = Files.readAllLines(Paths.get("./data/day1.txt"));
+        var list1 = new ArrayList<Integer>();
+        var list2 = new ArrayList<Integer>();
 
-    private int getOriginalNumber(String text) {
-        int[] digits = text.chars().filter(Character::isDigit).map(Character::getNumericValue).toArray();
-        return digits[0] * 10 + digits[digits.length - 1];
+        for (var line : lines) {
+            var numbers = line.split("   ");
+            list1.add(Integer.parseInt(numbers[0]));
+            list2.add(Integer.parseInt(numbers[1]));
+        }
+
+        Collections.sort(list1);
+        Collections.sort(list2);
+
+        int totalDistance = 0;
+
+        for (int i = 0; i < list1.size(); i++) {
+            totalDistance += Math.abs(list1.get(i) - list2.get(i));
+        }
+
+        return totalDistance;
     }
 
 }
