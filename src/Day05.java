b4715178273a61c790ba4ccb81d5e0f022ad8ad9
Javier Fernandez-Ivern <ivern@netflix.com>
2024-12-04 23:31:19 -0600

    Cleanup

--- src/Day05.java
+++ src/Day05.java
@@ -10,38 +10,15 @@ public class Day05 extends Day {
 
     @Override
     protected Long partOne(List<String> input) {
-        var lessThan = new HashMap<Long, HashSet<Long>>();
-        int line = 0;
-
-        while (!input.get(line).isBlank()) {
-            var parts = input.get(line++).split("\\|");
-            lessThan.computeIfAbsent(Long.parseLong(parts[0]), _k -> new HashSet<>())
-                    .add(Long.parseLong(parts[1]));
-        }
-
-        long sum = 0;
-
-        while (++line < input.size()) {
-            boolean ordered = true;
-            var parts = Arrays.stream(input.get(line).split(",")).mapToLong(Long::parseLong).toArray();
-            for (int i = 0; i < parts.length && ordered; i++) {
-                for (int j = i + 1; j < parts.length && ordered; j++) {
-                    if (lessThan.containsKey(parts[j]) && lessThan.get(parts[j]).contains(parts[i])) {
-                        ordered = false;
-                    }
-                }
-            }
-
-            if (ordered) {
-                sum += parts[parts.length / 2];
-            }
-        }
-
-        return sum;
+        return solve(input, 1);
     }
 
     @Override
     protected Long partTwo(List<String> input) {
+        return solve(input, 2);
+    }
+
+    private long solve(List<String> input, int part) {
         var lessThan = new HashMap<Long, HashSet<Long>>();
         int line = 0;
 
@@ -51,11 +28,12 @@ public class Day05 extends Day {
                     .add(Long.parseLong(parts[1]));
         }
 
-        long sum = 0;
+        long sum = 0L;
 
         while (++line < input.size()) {
+            var parts = Arrays.stream(input.get(line).split(",")).map(Long::parseLong).toArray(Long[]::new);
             boolean ordered = true;
-            Long[] parts = Arrays.stream(input.get(line).split(",")).map(Long::parseLong).toArray(Long[]::new);
+
             for (int i = 0; i < parts.length && ordered; i++) {
                 for (int j = i + 1; j < parts.length && ordered; j++) {
                     if (lessThan.containsKey(parts[j]) && lessThan.get(parts[j]).contains(parts[i])) {
@@ -64,7 +42,9 @@ public class Day05 extends Day {
                 }
             }
 
-            if (!ordered) {
+            if (part == 1 && ordered) {
+                sum += parts[parts.length / 2];
+            } else if (part == 2 && !ordered) {
                 Arrays.sort(parts, (x, y) -> {
                     if (lessThan.containsKey(x) && lessThan.get(x).contains(y)) {
                         return -1;
