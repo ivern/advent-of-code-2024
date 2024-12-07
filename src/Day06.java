d0828cb863f85b5243e707b64108689e15089b97
Javier Fernandez-Ivern <ivern@netflix.com>
2024-12-07 00:45:21 -0600

    Optimized day 6

--- src/Day06.java
+++ src/Day06.java
@@ -5,6 +5,7 @@ import util.Direction;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @SuppressWarnings("unused")
 public class Day06 extends Day {
@@ -14,9 +15,56 @@ public class Day06 extends Day {
 
     @Override
     protected Long partOne(List<String> input) {
-        var map = DenseGrid.fromInput(input).get(0);
+        var map = DenseGrid.fromInput(input).getFirst();
+
         var guardPosition = map.find(this::isGuard);
         var guard = new Guard(guardPosition, getDirection(map.get(guardPosition)));
+
+        return (long) getPath(map, guard).size();
+    }
+
+    @Override
+    protected Long partTwo(List<String> input) {
+        var map = DenseGrid.fromInput(input).getFirst();
+
+        var guardPosition = map.find(this::isGuard);
+        var guard = new Guard(guardPosition, getDirection(map.get(guardPosition)));
+        var initialPath = getPath(map, guard);
+
+        long options = 0;
+
+        for (var coordinate : initialPath) {
+            if (map.get(coordinate) != '.') {
+                continue;
+            }
+
+            map.put(coordinate, '#');
+
+            guard = new Guard(guardPosition, getDirection(map.get(guardPosition)));
+            var seen = new HashSet<Guard>();
+
+            while (map.contains(guard.coordinate())) {
+                if (seen.contains(guard)) {
+                    ++options;
+                    break;
+                } else {
+                    seen.add(guard);
+                }
+
+                var nextPosition = guard.coordinate().move(guard.direction());
+
+                guard = (map.contains(nextPosition) && map.get(nextPosition) == '#')
+                        ? new Guard(guard.coordinate(), guard.direction().turnClockwise())
+                        : new Guard(guard.coordinate().move(guard.direction()), guard.direction());
+            }
+
+            map.put(coordinate, '.');
+        }
+
+        return options;
+    }
+
+    private Set<Coordinate> getPath(DenseGrid<Character> map, Guard guard) {
         var seen = new HashSet<Coordinate>();
 
         while (map.contains(guard.coordinate())) {
@@ -29,40 +77,7 @@ public class Day06 extends Day {
                     : new Guard(guard.coordinate().move(guard.direction()), guard.direction());
         }
 
-        return (long) seen.size();
-    }
-
-    @Override
-    protected Long partTwo(List<String> input) {
-        return DenseGrid.fromInput(input).get(0).mapReduce((map, row, col) -> {
-            if (map.get(row, col) != '.') {
-                return 0L;
-            }
-
-            map.put(row, col, '#');
-
-            var guardPosition = map.find(this::isGuard);
-            var guard = new Guard(guardPosition, getDirection(map.get(guardPosition)));
-            var seen = new HashSet<Guard>();
-
-            while (map.contains(guard.coordinate())) {
-                if (seen.contains(guard)) {
-                    map.put(row, col, '.');
-                    return 1L;
-                } else {
-                    seen.add(guard);
-                }
-
-                var nextPosition = guard.coordinate().move(guard.direction());
-
-                guard = (map.contains(nextPosition) && map.get(nextPosition) == '#')
-                        ? new Guard(guard.coordinate(), guard.direction().turnClockwise())
-                        : new Guard(guard.coordinate().move(guard.direction()), guard.direction());
-            }
-
-            map.put(row, col, '.');
-            return 0L;
-        }, Long::sum, 0L);
+        return seen;
     }
 
     private boolean isGuard(char position) {
